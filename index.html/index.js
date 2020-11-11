73c9cf8c38a19809d7ca41066718ae26663fda7f
mygitcai <3607179539@qq.com>
2020-11-11 15:11:19 +0800

    日志

---
+++ index.html/index.js
@@ -0,0 +1,8 @@
+function apple(){
+    var array = [12,34,45,34,24,15,67];
+    for(let index = 0;index < array.length;index++){
+        const element = array[index];
+        console.log(element);
+    }
+}
+apple();
